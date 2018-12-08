99088d94b68876eda921cb39e524e2a6e1dc861c
Hades <markus_hadinger@yahoo.de>
2018-12-08 13:12:55 +0100

    resources

---
+++ Lego Bewego/Assets/scripts/StartStoneHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartStoneHelper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().velocity=new Vector3(0, 0, 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
