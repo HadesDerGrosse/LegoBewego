6f9fcc39584a6fb88a45de420310e0f28cd825a2
Christoph Lendenfeld <chris.lend@gmx.at>
2018-12-09 17:49:50 +0100

    main scene and ui

---
+++ Lego Bewego/Assets/scripts/StoryManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryManager : MonoBehaviour {
+
+    public Image[] characters;
+    public string[] story;
+
+    public Image storyguy;
+    public Text storyText;
+    public RectTransform parentTransform;
+
+    private bool moving = false;
+    private bool showing = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("s")) toggleStory();
+	}
+
+    public void toggleStory()
+    {
+        parentTransform.GetComponent<Animation>().Play();
+    }
+
+}
